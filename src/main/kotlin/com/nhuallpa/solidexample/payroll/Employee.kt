9555bfaebc40f432c2bc154fd10c333cd6df9bf3
nestor.huallpa <huallpa.nestor@gmail.com>
2024-05-08 20:50:25 -0300

    Complete employee class with SRP problem

--- src/main/kotlin/com/nhuallpa/solidexample/payroll/Employee.kt
+++ src/main/kotlin/com/nhuallpa/solidexample/payroll/Employee.kt
@@ -2,27 +2,42 @@ package com.nhuallpa.solidexample.payroll
 
 private const val NORMAL_HOURS = 160
 
+private const val RATE_USD: Double = 30.0
+
 class Employee (name:String, cuit:String){
 
     private fun getTimeOffInHours() = 8
 
-    fun calculatePay(): Int {
-        return 30 * workingHours();
+    fun calculatePay(): Double {
+        print("Apply business rules")
+        return processPayment(retrieveRate(), retrieveWorkingHours())
     }
 
-    fun reportHours(): Int {
-        return convertToExcel(workingHours())
+    fun reportHours(): String {
+        print("Calling third-party library")
+        return buildReport(retrieveWorkingHours())
     }
 
     fun save() {
+        saveWithEntityManager(this)
+    }
+
+    private fun saveWithEntityManager(employee: Employee) {
         print("Save data in the database")
     }
 
-    private fun convertToExcel(workingHours: Int): Int {
-        TODO("Not yet implemented")
+    private fun buildReport(workingHours: Int): String {
+        print("Calling third-party library to generate report")
+        return "Report was built successful"
     }
 
-    private fun workingHours():Int {
-        return NORMAL_HOURS - getTimeOffInHours();
+    private fun processPayment(retrieveRate: Double, retrieveWorkingHours: Int): Double {
+        return retrieveRate() * retrieveWorkingHours();
+    }
+
+    private fun retrieveRate() = RATE_USD
+
+    private fun retrieveWorkingHours():Int {
+        return NORMAL_HOURS
     }
 }
