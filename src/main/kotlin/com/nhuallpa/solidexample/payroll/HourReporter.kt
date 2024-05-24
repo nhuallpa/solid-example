6cc82367ded621b5f6edf031073011bb0fcdf24f
nestor.huallpa <huallpa.nestor@gmail.com>
2024-05-24 16:45:43 -0300

    Update HourReporter to show problems

--- src/main/kotlin/com/nhuallpa/solidexample/payroll/HourReporter.kt
+++ src/main/kotlin/com/nhuallpa/solidexample/payroll/HourReporter.kt
@@ -2,13 +2,30 @@ package com.nhuallpa.solidexample.payroll
 
 class HourReporter {
 
-    fun reportHours(employee: Employee): String {
-        print("Calling third-party library")
-        return buildReport(employee.retrieveWorkingHours())
+    fun reportHours(employee: Employee) {
+        // 1- Retrieve info from database
+        val employeePayrollDetail =  retrievePayrollDetail(employee)
+        // 2- Apply business logic
+        val totalWorkingHours = computeTotalWorkingHour(employeePayrollDetail)
+        // 3- Print the report in PDF
+        printReport(employee, employeePayrollDetail, totalWorkingHours)
     }
 
-    private fun buildReport(workingHours: Int): String {
-        print("Calling third-party library to generate report")
+    private fun computeTotalWorkingHour(employeePayrollDetail: EmployeePayrollDetail): Int {
+        return employeePayrollDetail.workingDailyHours.sum()
+    }
+
+    private fun retrievePayrollDetail(employee: Employee): EmployeePayrollDetail {
+        print("Retrieve Payroll information for $employee")
+        return EmployeePayrollDetail(listOf())
+    }
+
+    private fun printReport(employee: Employee, employeePayrollDetail: EmployeePayrollDetail, totalWorkingHours: Int): String {
+        printPdf()
         return "Report was built successful"
+    }
+
+    private fun printPdf() {
+        print("Calling third-party library to generate report")
     }
 }
